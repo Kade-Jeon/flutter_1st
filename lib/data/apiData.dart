50ce6cf1c094706d88c73739163a0851f90b49ae
kade <zsc0248@gmail.com>
2025-01-01 20:36:28 +0900

    데이터 추가 테스트 (일본 엔)

--- lib/data/apiData.dart
+++ lib/data/apiData.dart
@@ -16,10 +16,16 @@ const apiData = [
     "code": "USD",
     "amount": "9 130",
   },
+  {
+    "name": "Yen",
+    "code": "JPY",
+    "amount": "98 612 301 900",
+  },
 ];
 
 final Map<String, IconData> iconMap = {
   'EUR': Icons.euro_rounded,
   'BTC': Icons.currency_bitcoin_rounded,
   'USD': Icons.attach_money_rounded,
+  'JPY': Icons.currency_yen_rounded,
 };
